b84438dc4b192bd3772773e56d27780e4ee5b354
Krish Furia <krishfuria13@users.noreply.github.com>
2015-12-22 19:09:07 -0800

    Initial Commit

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -9,10 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timer  = NSTimer()
+    
+    var hour = 2
+    var min = 58
+    var sec = 55
+    
+    @IBOutlet var timerLabel: UILabel!
+    
+    @IBOutlet var playButtonOutlet: UIBarButtonItem!
+    
+    @IBAction func playButton(sender: AnyObject) {
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("increaseTimer"), userInfo: nil, repeats: true)
+        playButtonOutlet.enabled = false
+    }
+    
+    @IBAction func reset(sender: AnyObject) {
+        timer.invalidate()
+        sec = 0;
+        min = 0;
+        hour = 0;
+        timerLabel.text = "00:00:00"
+        playButtonOutlet.enabled = true
+    }
+    
+    @IBAction func pauseButton(sender: AnyObject) {
+        timer.invalidate();
+        playButtonOutlet.enabled = true
+    }
+    
 
+    func increaseTimer() {
+        sec++
+        if sec > 59 {
+            if min < 59 {
+                min++
+                sec = 0
+            }
+            else {
+                hour++
+                min = 0
+                sec = 0
+            }
+        }
+        timerLabel.text = String(format: "%02d", hour) + ":" + String(format: "%02d", min) + ":" + String(format: "%02d", sec)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
